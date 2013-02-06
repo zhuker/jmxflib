b34ad0ffc70aef848d1a49adce57a794396502c9
zhukov.alex@gmail.com <zhukov.alex@gmail.com@d9db98d4-90d5-b36b-c000-715b551fae7b>
2013-02-06 01:39:28 +0000

    JPEG2000FrameWrappedPictureElement

--- trunk/src/test/java/com/vg/mxf/KeyTest.java
+++ trunk/src/test/java/com/vg/mxf/KeyTest.java
@@ -1,5 +1,8 @@
 package com.vg.mxf;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,11 +11,22 @@ public class KeyTest {
     public void testMatches() throws Exception {
         Key dbk = Key.key("06.0E.2B.34.02.05.01.01.0D.01.02.01.01.02.04.00", 0xf00);
         Key ink = Key.key("06.0E.2B.34.02.05.01.01.0D.01.02.01.01.02.02.00");
-        Assert.assertTrue(ink.matches(dbk));
-        Assert.assertTrue(dbk.matches(ink));
+        assertTrue(ink.matches(dbk));
+        assertTrue(dbk.matches(ink));
         dbk = Key.key("06.0E.2B.34.02.05.01.01.0D.01.02.01.01.02.04.00");
         ink = Key.key("06.0E.2B.34.02.05.01.01.0D.01.02.01.01.02.02.00");
-        Assert.assertFalse(dbk.matches(ink));
+        assertFalse(dbk.matches(ink));
     }
+    
+    @Test
+    public void testJ2KFrame() throws Exception {
+        Key j2kFrameStrict = Key.key("06.0E.2B.34.01.02.01.01.0D.01.03.01.15.01.08.00");
+        Key j2kFrame = Key.key("06.0E.2B.34.01.02.01.01.0D.01.03.01.15.01.08.00", 0xff);
+        Key in = Key.key("06.0E.2B.34.01.02.01.01.0D.01.03.01.15.01.08.01");
+        assertTrue(j2kFrame.matches(in));
+        assertFalse(j2kFrameStrict.matches(in));
+    }
+    
+    
 
 }
