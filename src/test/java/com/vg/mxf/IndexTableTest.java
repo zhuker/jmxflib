e602eb2207f80d9b1904b5332c78da1fc4c87b2b
zhukov.alex@gmail.com <zhukov.alex@gmail.com@d9db98d4-90d5-b36b-c000-715b551fae7b>
2013-10-08 14:29:40 +0000

    0.0.12 index table bug fixed

--- src/test/java/com/vg/mxf/IndexTableTest.java
+++ src/test/java/com/vg/mxf/IndexTableTest.java
@@ -1,12 +1,17 @@
 package com.vg.mxf;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.nio.ByteBuffer;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import com.vg.io.SeekableFileInputStream;
+import com.vg.io.SeekableInputStream;
+import com.vg.mxf.Registry.ULDesc;
 
 public class IndexTableTest {
     @Test
@@ -39,6 +44,64 @@ public class IndexTableTest {
         createTable.write(bout);
         int size = bout.size();
         Assert.assertEquals(2200120, size);
+    }
+
+    @Test
+    public void testIMF() throws Exception {
+        File f = new File(
+                "/Users/zhukov/from_colorworks/20131007/ELDORADO/IMF/VOFELD_V3_20130412_OV/vof_el_dorado_v06_00_20120329_c06_grd18_REC709_noTrim_EXR_wCAT_00.mxf");
+        File f_ = new File(
+                "/Users/zhukov/testdata/j2k/hd/stem/media/StemTape_2010_HD_16x9_240_2398_english_0630_JPEG2000_v0.mxf");
+        SeekableInputStream in = new SeekableFileInputStream(f);
+        MxfStructure mxf = MxfStructure.readStructure(in);
+        long frameStreamOffset = mxf.getFrameStreamOffset(241);
+        in.close();
+        in = new SeekableFileInputStream(f);
+        long length = in.length();
+        int fn = 0;
+        while (length > in.position()) {
+            long pos = in.position();
+            KLV klv = KLV.readKL(in);
+            boolean skip = true;
+            // if (BodyPartitionPack.Key.matches(klv.key)) {
+            // System.out.println(pos);
+            // skip = false;
+            // byte b[] = new byte[(int) klv.len];
+            // Assert.assertEquals(b.length, in.read(b));
+            // ByteBuffer wrap = ByteBuffer.wrap(b);
+            // BodyPartitionPack it = new BodyPartitionPack();
+            // it.setByteBuffer(wrap, 0);
+            // it.parse();
+            // System.out.println(it.toDebugString());
+            //
+            // }
+            // if (IndexTable.Key.matches(klv.key)) {
+            // skip = false;
+            // byte b[] = new byte[(int) klv.len];
+            // Assert.assertEquals(b.length, in.read(b));
+            // ByteBuffer wrap = ByteBuffer.wrap(b);
+            // IndexTable it = new IndexTable();
+            // it.setByteBuffer(wrap, 0);
+            // it.parse();
+            // // System.out.println(it.toDebugString());
+            // }
+            if (Registry.JPEG2000FrameWrappedPictureElement.matches(klv.key)) {
+                long idxpos = mxf.getFrameStreamOffset(fn);
+                System.out.printf("%3d %10d %10d %10d\n", fn, pos, idxpos, (idxpos - pos));
+                fn++;
+            } else {
+                System.out.println(klv);
+
+            }
+            if (skip) {
+                in.skip(klv.len);
+            }
+            if (fn > 1000) {
+                break;
+            }
+        }
+        in.close();
+
     }
 
 }
